71eaa8d97150a5affa8a2a03a763c2c33b98bc8c
eugenel97 <eugenejr14@gmail.com>
2025-01-10 07:39:48 -0800

    Bars displaying to screen properly; no sorting yet

--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
 
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    sf::RenderWindow window(sf::VideoMode(WIDTH,HEIGHT), "Algorithm Visualizer");
 
-    for (int i = 1; i <= 5; i++) {
+    const int MAX_BARS = 100;
+    const float BAR_WIDTH = WIDTH / MAX_BARS;
 
-        std::cout << "i = " << i << std::endl;
+    std::vector<sf::RectangleShape> bars (MAX_BARS);
+    std::vector<int> heights (MAX_BARS);
+
+    for (int i = 0; i < MAX_BARS; ++i) {
+        heights[i] = rand() % HEIGHT;
+        bars[i].setSize(sf::Vector2f{BAR_WIDTH, static_cast<float>(heights[i])});
+        bars[i].setPosition(i * BAR_WIDTH, HEIGHT - static_cast<float>(heights[i]));
+        bars[i].setFillColor(sf::Color::White);
+    }
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear(sf::Color::Black);
+        for (auto bar: bars) {
+            window.draw(bar);
+        }
+        window.display();
     }
 
     return 0;
