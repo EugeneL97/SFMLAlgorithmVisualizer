9aa07c4ee23e92fbbdccc9cb2e6b5da8e7cfa499
eugenel97 <eugenejr14@gmail.com>
2025-01-23 05:56:17 -0800

    Rough list of all sorting algorithms to implement

--- Sorting.cpp
+++ Sorting.cpp
@@ -4,14 +4,17 @@
 #include "Sorting.h"
 
 Sorting::Sorting(int maxBars, int screenWidth, int screenHeight, Render &render)
-: maxBars(maxBars), screenWidth(screenWidth), screenHeight(screenHeight), render(render) {
+: maxBars(maxBars), screenWidth(screenWidth), screenHeight(screenHeight), render(render)
+{
     barWidth = static_cast<float>(screenWidth) / maxBars;
     bars.resize(maxBars);
     heights.resize(maxBars);
 }
 
-void Sorting::initializeBars() {
-    for (int i = 0; i < maxBars; ++i) {
+void Sorting::initializeBars()
+{
+    for (int i = 0; i < maxBars; ++i)
+        {
         heights[i] = rand() % screenHeight;
         bars[i].setSize(sf::Vector2f{barWidth, (heights[i])});
         bars[i].setPosition(i * barWidth, screenHeight - heights[i]);
@@ -19,14 +22,19 @@ void Sorting::initializeBars() {
     }
 }
 
-void Sorting::visualizeBubbleSort(sf::RenderWindow& window) {
+void Sorting::bubbleSort(sf::RenderWindow& window)
+{
     initializeBars();
-    for (int i = 0; i < maxBars; ++i) {
-        for (int j = 0; j < maxBars - 1; ++j) {
-            if (!render.handleWindowEvents(window)) {
+    for (int i = 0; i < maxBars; ++i)
+    {
+        for (int j = 0; j < maxBars - 1; ++j)
+        {
+            if (!render.handleWindowEvents(window))
+            {
                 return;
             }
-            if (heights[j] > heights[j + 1]) {
+            if (heights[j] > heights[j + 1])
+            {
                 std::swap(heights[j], heights[j + 1]);
 
                 bars[j].setSize(sf::Vector2f{barWidth, (heights[j])});
@@ -46,5 +54,52 @@ void Sorting::visualizeBubbleSort(sf::RenderWindow& window) {
             }
         }
     }
+    render.waitForUserInput(window);
+}
+
+void Sorting::selectionSort(sf::RenderWindow &window)
+{
+
+}
+
+void Sorting::insertionSort(sf::RenderWindow &window)
+{
+
+}
+
+void Sorting::mergeSort(sf::RenderWindow &window)
+{
+
+}
+
+void Sorting::heapSort(sf::RenderWindow &window)
+{
+
+}
+
+void Sorting::quickSort(sf::RenderWindow &window)
+{
+
+}
+
+void Sorting::countingSort(sf::RenderWindow &window)
+{
+
+}
+
+void Sorting::radixSort(sf::RenderWindow &window)
+{
+
+}
+
+void Sorting::bucketSort(sf::RenderWindow &window)
+{
+
+}
+
+void Sorting::bogoSort(sf::RenderWindow& window)
+{
+    initializeBars();
+    
     render.waitForUserInput(window);
 }
--- Sorting.h
+++ Sorting.h
@@ -4,7 +4,8 @@
 
 #include "Render.h"
 
-class Sorting {
+class Sorting
+{
     std::vector<sf::RectangleShape> bars;
     std::vector<float> heights;
     int maxBars;
@@ -14,5 +15,14 @@ class Sorting {
 public:
     Sorting(int maxBars, int screenWidth, int screenHeight, Render& render);
     void initializeBars();
-    void visualizeBubbleSort(sf::RenderWindow& window);
+    void bubbleSort(sf::RenderWindow& window);
+    void selectionSort(sf::RenderWindow& window);
+    void insertionSort(sf::RenderWindow& window);
+    void mergeSort(sf::RenderWindow& window);
+    void heapSort(sf::RenderWindow& window);
+    void quickSort(sf::RenderWindow& window);
+    void countingSort(sf::RenderWindow& window);
+    void radixSort(sf::RenderWindow& window);
+    void bucketSort(sf::RenderWindow& window);
+    void bogoSort(sf::RenderWindow& window);
 };
